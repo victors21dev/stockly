cff9ea1589ee3cf1179323479cc2da33b659867c
Victor Santos <santos.victors2000@gmail.com>
2026-01-26 19:34:49 -0300

    refactor: add route handlers

---
+++ app/api/products/route.ts
@@ -0,0 +1,28 @@
+import { db } from '@/app/_lib/prisma'
+
+// APENAS PARA REFERÊNCIA
+
+export async function GET() {
+  const products = await db.product.findMany({})
+
+  return Response.json(products, {
+    status: 200,
+  })
+}
+
+export async function POST(request: Request) {
+  const body = await request.json()
+
+  const name = body.name
+  const price = body.price
+  const stock = body.stock
+
+  await db.product.create({
+    data: {
+      name,
+      price,
+      stock,
+    },
+  })
+  return Response.json({}, { status: 201 })
+}
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -21,7 +21,10 @@ const ProductsPage = async () => {
           Novo produto
         </Button>
       </div>
-      <DataTable columns={productTableColumns} data={products} />
+      <DataTable
+        columns={productTableColumns}
+        data={JSON.parse(JSON.stringify(products))}
+      />
     </div>
   )
 }
